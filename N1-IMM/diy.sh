90e7ab235fd6464fb19e712391fc2fce5b771234
xysck <xysck@126.com>
2025-10-01 18:11:35 +0800

    Update diy.sh

--- N1-IMM/diy.sh
+++ N1-IMM/diy.sh
@@ -13,7 +13,7 @@ function git_sparse_clone() {
 sed -i 's/192.168.1.1/192.168.1.254/g' package/base-files/files/bin/config_generate
 
 # Add packages
-#添加科学上网源
+# 添加科学上网源
 git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 #git clone --depth 1 https://github.com/ophub/luci-app-amlogic package/amlogic
@@ -22,7 +22,7 @@ git clone --depth 1 https://github.com/sbwml/luci-app-mosdns package/mosdns
 #git clone --depth 1 https://github.com/sbwml/luci-app-alist package/alist
 #git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall
 
-#删除库中的插件，使用自定义源中的包。
+# 删除库中的插件，使用自定义源中的包。
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-argon-config
 #rm -rf feeds/luci/applications/luci-app-ddns-go
@@ -40,6 +40,9 @@ sed -i '/"VPN"/d' ./feeds/luci/applications/luci-app-zerotier/luasrc/controller/
 sed -i 's/vpn/services/g' ./feeds/luci/applications/luci-app-zerotier/luasrc/controller/zerotier.lua
 sed -i 's/vpn/services/g' ./feeds/luci/applications/luci-app-zerotier/luasrc/view/zerotier/zerotier_status.htm
 
-#替换luci-app-socat为https://github.com/chenmozhijin/luci-app-socat
+# 替换luci-app-socat为https://github.com/chenmozhijin/luci-app-socat
 #rm -rf feeds/luci/applications/luci-app-socat
 #git_sparse_clone main https://github.com/chenmozhijin/luci-app-socat luci-app-socat
+
+# 添加旁路由的防火墙规则
+sed -i "\$a echo 'iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE' >> /etc/firewall.user" ./package/lean/default-settings/files/zzz-default-settings
