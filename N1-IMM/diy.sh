edf3eeb8085e865ea7f2fd105f0a74e7f5c80674
xysck <xysck@126.com>
2025-10-05 07:57:18 +0800

    Update diy.sh

--- N1-IMM/diy.sh
+++ N1-IMM/diy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
   branch="$1" repourl="$2" && shift 2
@@ -10,41 +11,22 @@ function git_sparse_clone() {
 }
 
 # Add packages
-# 添加科学上网源
-git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-#git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-#git clone --depth 1 https://github.com/ophub/luci-app-amlogic package/amlogic
-#git clone --depth 1 https://github.com/sirpdboy/luci-app-ddns-go package/ddnsgo
-git clone --depth 1 https://github.com/sbwml/luci-app-mosdns package/mosdns
-#git clone --depth 1 https://github.com/sbwml/luci-app-alist package/alist
-#git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall
+#git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+#git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
+#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-aliddns
+#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-pushbot
+#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-turboacc
 
-# 删除库中的插件，使用自定义源中的包。
-rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/luci/applications/luci-app-argon-config
-#rm -rf feeds/luci/applications/luci-app-ddns-go
-#rm -rf feeds/packages/net/ddns-go
-#rm -rf feeds/packages/net/alist
-#rm -rf feeds/luci/applications/luci-app-alist
-#rm -rf feeds/luci/applications/openwrt-passwall
+# 加入OpenClash核心
+#chmod -R a+x $GITHUB_WORKSPACE/preset-clash-core.sh
+#$GITHUB_WORKSPACE/N1/preset-clash-core.sh
 
-# 替换luci-app-openvpn-server imm源的启动不了服务！
-#rm -rf feeds/luci/applications/luci-app-openvpn-server
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-openvpn-server
-
-# 调整 zerotier 到 服务 菜单
-sed -i '/"VPN"/d' ./feeds/luci/applications/luci-app-zerotier/luasrc/controller/zerotier.lua
-sed -i 's/vpn/services/g' ./feeds/luci/applications/luci-app-zerotier/luasrc/controller/zerotier.lua
-sed -i 's/vpn/services/g' ./feeds/luci/applications/luci-app-zerotier/luasrc/view/zerotier/zerotier_status.htm
-
-# 替换luci-app-socat为https://github.com/chenmozhijin/luci-app-socat
-#rm -rf feeds/luci/applications/luci-app-socat
-#git_sparse_clone main https://github.com/chenmozhijin/luci-app-socat luci-app-socat
-
-# 修改默认IP、主机名
+# 修改默认IP
 sed -i 's/192.168.1.1/192.168.1.254/g' package/base-files/files/bin/config_generate
-sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 
 # 修改默认主题
 sed -i 's/luci-theme-design/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# 修改主机名
+sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
