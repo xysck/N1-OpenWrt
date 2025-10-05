83434067f9b66d8ba9de4a9f786b24f3f9b7d93d
xysck <xysck@126.com>
2025-10-06 07:41:13 +0800

    Update diy.sh

--- N1-IMM/diy.sh
+++ N1-IMM/diy.sh
@@ -19,10 +19,12 @@ git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
 git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
 git clone --depth=1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
-
 git clone --depth=1 https://github.com/DustReliant/luci-app-filetransfer package/luci-app-filetransfer
 git_sparse_clone main https://github.com/kiddin9/kwrt-packages ddns-go
 git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-ddns-go
+
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-fileassistant
+git clone --depth=1 https://github.com/fw876/helloworld package/helloworld
 
 # 加入OpenClash核心
 #chmod -R a+x $GITHUB_WORKSPACE/preset-clash-core.sh
