985b5eb759cf4d1b48a77aed6449f0cc561ebf9e
xysck <xysck@126.com>
2025-10-06 07:48:32 +0800

    Update diy.sh

--- N1-LEDE/diy.sh
+++ N1-LEDE/diy.sh
@@ -10,10 +10,7 @@ function git_sparse_clone() {
 }
 
 # Add packages
-# 添加科学上网源
-#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
-#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
-git clone --depth=1 https://github.com/kenzok8/small package/small
+# 添加自定义的软件包源
 git clone --depth=1 https://github.com/kenzok8/openwrt-packages package/openwrt-packages
 git clone -b 18.06 --single-branch --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone -b 18.06 --single-branch --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
@@ -23,9 +20,10 @@ git clone -b v5-lua --single-branch --depth 1 https://github.com/sbwml/luci-app-
 git clone -b lua --single-branch --depth 1 https://github.com/sbwml/luci-app-alist package/alist
 git clone --depth=1 https://github.com/gdy666/luci-app-lucky.git package/lucky
 
-# 添加自定义的软件包源
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages ddns-go
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-ddns-go
+# 添加科学上网源
+#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
+#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+git clone --depth=1 https://github.com/kenzok8/small package/small
 
 # Remove packages
 # 删除lean库中的插件，使用自定义源中的包。
