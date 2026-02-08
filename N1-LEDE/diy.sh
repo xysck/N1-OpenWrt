e450b1caeeed46a2ecbad92e90c77986712ae099
xysck <xysck@126.com>
2026-02-08 10:29:49 +0800

    Update diy.sh

--- N1-LEDE/diy.sh
+++ N1-LEDE/diy.sh
@@ -58,7 +58,10 @@ sed -i 's/bootstrap/argone/g' feeds/luci/collections/luci-ssl-nginx/Makefile
 sed -i 's/bootstrap/argone/g' feeds/luci/collections/luci-nginx/Makefile
 
 # #添加旁路由的防火墙规则
-sed -i "\$i echo 'iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE' >> /etc/firewall.user\n" ./package/lean/default-settings/files/zzz-default-settings
+sed -i '\$i echo 'iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE' >> /etc/firewall.user\n' ./package/lean/default-settings/files/zzz-default-settings
+
+# 修改默认时间格式
+sed -i 's/os.date()/os.date('%Y-%m-%d %H:%M:%S %A')/g' package/lean/autocore/files/*/index.htm
 
 # 添加 OpenClash Meta 内核
 mkdir -p files/etc/openclash/core
@@ -67,3 +70,6 @@ tar -zxvf "clash_meta.tar.gz" -C files/etc/openclash/core/
 mv files/etc/openclash/core/clash files/etc/openclash/core/clash_meta
 chmod +x files/etc/openclash/core/clash_meta
 rm -f "clash_meta.tar.gz"
+
+./scripts/feeds update -a
+./scripts/feeds install -a
