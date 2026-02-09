84e5d453ab8b9a1c180cd4ff75f32def5a9420dd
xysck <xysck@126.com>
2026-02-09 11:11:14 +0800

    Update diy.sh

--- N1-LEDE/diy.sh
+++ N1-LEDE/diy.sh
@@ -53,9 +53,9 @@ sed -i 's/vpn/services/g' feeds/luci/applications/luci-app-zerotier/luasrc/contr
 sed -i 's/vpn/services/g' feeds/luci/applications/luci-app-zerotier/luasrc/view/zerotier/zerotier_status.htm
 
 # 修改默认主题为argone
-sed -i 's/bootstrap/argone/g' feeds/luci/collections/luci/Makefile
-sed -i 's/bootstrap/argone/g' feeds/luci/collections/luci-ssl-nginx/Makefile
-sed -i 's/bootstrap/argone/g' feeds/luci/collections/luci-nginx/Makefile
+sed -i "s/bootstrap/argone/g" feeds/luci/collections/luci/Makefile
+sed -i "s/bootstrap/argone/g" feeds/luci/collections/luci-ssl-nginx/Makefile
+sed -i "s/bootstrap/argone/g" feeds/luci/collections/luci-nginx/Makefile
 
 # #添加旁路由的防火墙规则
 sed -i "\$i echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> /etc/firewall.user\n" ./package/lean/default-settings/files/zzz-default-settings
